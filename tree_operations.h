b061f430c2f0f37f14bd4529cd1c1fe9193c4006
Zsweetieszt <dinanda.kh@gmail.com>
2025-06-07 15:39:14 +0700

    Declare file I/O and database management functions

--- tree_operations.h
+++ tree_operations.h
@@ -12,4 +12,9 @@ void choice(TreeNodePtr start, TreeNodePtr* last_accessed);
 int ask_if_animal(TreeNodePtr start);
 void build_question(TreeNodePtr start);
 
+// Fungsi untuk File I/O dan manajemen database
+void write_to_file(TreeNodePtr start, FILE* out_file);
+int read_from_file(TreeNodePtr* start, FILE* in_file);
+int auto_save_tree(TreeNodePtr start);
+int load_or_create_database(TreeNodePtr* head);
 #endif // TREE_OPERATIONS_H
