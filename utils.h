b64d78dba14622e9b5772ec137b0713c6b24cc66
Zsweetieszt <dinanda.kh@gmail.com>
2025-06-08 12:06:49 +0700

    Declare advanced input validation and time utility functions

--- utils.h
+++ utils.h
@@ -32,4 +32,15 @@ void display_learning_analytics();
 void show_help_information();
 void display_session_summary();
 
+// --- FUNGSI VALIDASI INPUT BARU ---
+int get_valid_integer(int min, int max);
+void get_valid_string(char* buffer, int max_length, const char* prompt);
+
+// --- FUNGSI TAMPILAN BARU ---
+void display_system_status();
+
+// --- FUNGSI UTILITAS WAKTU BARU ---
+char* format_timestamp(time_t timestamp);
+int get_days_since_last_play();
+
 #endif // UTILS_H
