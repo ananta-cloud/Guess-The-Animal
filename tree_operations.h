83c9952e44e18e25f113aba6a5d8da97d2fec0ce
Zsweetieszt <dinanda.kh@gmail.com>
2025-06-08 15:23:44 +0700

    Finalize all tree operation function declarations

--- tree_operations.h
+++ tree_operations.h
@@ -3,25 +3,32 @@
 
 #include "common_types.h"
 
-// Fungsi untuk manajemen tree dasar
+// Tree management functions
 TreeNodePtr create_default_tree();
 void free_tree(TreeNodePtr start);
+TreeNodePtr copy_node(TreeNodePtr original);
 
-// Fungsi untuk File I/O dan manajemen database
+// File I/O operations
 void write_to_file(TreeNodePtr start, FILE* out_file);
 int read_from_file(TreeNodePtr* start, FILE* in_file);
 int auto_save_tree(TreeNodePtr start);
-int load_or_create_database(TreeNodePtr* head);
 int create_backup();
+int load_or_create_database(TreeNodePtr* head);
 
-// Fungsi untuk logika inti permainan
+// Game logic functions
 void choice(TreeNodePtr start, TreeNodePtr* last_accessed);
 int ask_if_animal(TreeNodePtr start);
 void build_question(TreeNodePtr start);
 
-// --- FUNGSI ANALISIS TREE BARU ---
+// Tree analysis functions
+void display_all_animals(TreeNodePtr root);
+void show_learning_path(TreeNodePtr root, char* target_animal);
 int count_total_animals(TreeNodePtr root);
 int calculate_tree_depth(TreeNodePtr root);
-void display_all_animals(TreeNodePtr root);
-void iterative_preorder_traversal(TreeNodePtr root); 
-#endif // TREE_OPERATIONS_H
+void find_most_asked_questions(TreeNodePtr root);
+
+// Tree traversal with stack (iterative)
+void iterative_preorder_traversal(TreeNodePtr root);
+void iterative_inorder_traversal(TreeNodePtr root);
+
+#endif
